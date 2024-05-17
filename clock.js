d81a48af8425126a1df32e70e9ecd905dfcc3416
maxsheridan <max@maxsheridan.com>
2024-05-17 16:03:08 +0300

    Update clock js, index

--- clock.js
+++ clock.js
@@ -2,8 +2,9 @@ function updateClock() {
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
-  const date = now.toDateString(); // Get the current date
-  const timeString = `${date} ${hours}:${minutes}`; // Combine date and time
+  const dateOptions = { year: 'numeric', month: 'short', day: 'numeric', weekday: 'short' };
+  const date = now.toLocaleDateString('ja-JP', dateOptions) + ' '; // Adding a space after the date
+  const timeString = `${date} ${hours}:${minutes}`;
   document.getElementById('clock').textContent = timeString;
 }
 
