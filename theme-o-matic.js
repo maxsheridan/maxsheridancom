5116f345f2a8c56df01ac448950f0310bd38e6be
maxsheridan <max@maxsheridan.com>
2025-02-03 10:09:47 +0200

    Revert

--- theme-o-matic.js
+++ theme-o-matic.js
@@ -21,12 +21,6 @@ document.querySelector('#theme-o-matic').addEventListener('click', () => {
 });
 
 window.addEventListener('DOMContentLoaded', applySavedMode);
-window.addEventListener('load', () => {
-    // Ensure all styles are loaded before applying theme
-    setTimeout(() => {
-        applySavedMode();
-    }, 100); // Adjust the delay as needed
-});
 window.addEventListener('unload', () => {
     document.documentElement.removeAttribute('data-force-color-mode');
 });
